9ea1166a36ce4486ac2322069ce943aa3c1cc47d
Carlos Anyel <123602429+nyelkk@users.noreply.github.com>
2024-02-16 13:34:02 -0300

    Large Update -

--- js/script.js
+++ js/script.js
@@ -1,38 +1,28 @@
-// Slider 1
-const slides1 = document.querySelector(".slides:nth-of-type(1)");
-const totalSlides1 = slides1.children.length;
-let index1 = 0;
-
-function nextSlide1() {
-  index1 = (index1 + 1) % totalSlides1;
-  updateSlide1();
+function prevSlide(sliderId) {
+    const slider = document.getElementById(sliderId);
+    const slides = slider.querySelector('.slides');
+    const slideWidth = slides.querySelector('.slide').offsetWidth;
+    slides.style.transform = `translateX(-${slideWidth}px)`;
+    setTimeout(() => {
+        slides.appendChild(slides.firstElementChild);
+        slides.style.transition = 'none';
+        slides.style.transform = 'translateX(0)';
+        setTimeout(() => {
+            slides.style.transition = 'transform 0.5s ease';
+        });
+    }, 500);
 }
 
-function prevSlide1() {
-  index1 = (index1 - 1 + totalSlides1) % totalSlides1;
-  updateSlide1();
+function nextSlide(sliderId) {
+    const slider = document.getElementById(sliderId);
+    const slides = slider.querySelector('.slides');
+    const slideWidth = slides.querySelector('.slide').offsetWidth;
+    slides.style.transition = 'transform 0.5s ease';
+    slides.style.transform = `translateX(-${slideWidth}px)`;
+    setTimeout(() => {
+        slides.appendChild(slides.firstElementChild);
+        slides.style.transition = 'none';
+        slides.style.transform = 'translateX(0)';
+    }, 500);
 }
 
-function updateSlide1() {
-  const offset1 = -index1 * 100;
-  slides1.style.transform = `translateX(${offset1}%)`;
-}
-
-const slides2 = document.querySelector(".slides:nth-of-type(2)");
-const totalSlides2 = slides2.children.length;
-let index2 = 0;
-
-function nextSlide2() {
-  index2 = (index2 + 1) % totalSlides2;
-  updateSlide2();
-}
-
-function prevSlide2() {
-  index2 = (index2 - 1 + totalSlides2) % totalSlides2;
-  updateSlide2();
-}
-
-function updateSlide2() {
-  const offset2 = -index2 * 100;
-  slides2.style.transform = `translateX(${offset2}%)`;
-}
